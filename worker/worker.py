1be0583b6ecaf7621b665af058acad72eaa2ea46
Zar Chi <zarc5204@gmail.com>
2025-12-30 05:01:12 +0800

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -5,10 +5,10 @@ from workflows.jobs_workflow import JobWorkflow
 from worker.activities import compute_activity
 
 async def main():
-    # Connect to Temporal server
+    
     client = await Client.connect("localhost:7233")
 
-    # Start a worker listening to "jobs-task-queue"
+    
     worker = Worker(
         client,
         task_queue="jobs-task-queue",
